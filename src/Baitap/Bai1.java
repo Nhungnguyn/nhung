520d95870236be3fb91964c4e2bff984cc01e9b7
nhung_23it.eb068 <nhungnth.23ite@vku.udn.vn>
2024-04-10 12:20:03 +0700

    first commit

--- src/Baitap/Bai1.java
+++ src/Baitap/Bai1.java
@@ -1,5 +1,31 @@
 package Baitap;
 
-public class Bai1 {
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
+public class Bai1 {
+    private static String bytesToHex( byte[]hash){
+        StringBuilder hexString = new StringBuilder();
+        for (byte b: hash){
+            String hex= Integer.toHexString(0xff & b);
+            if (hex.length()==1){
+                hexString.append('0');
+            }
+            hexString.append(hex);
+        }
+        return hexString.toString();
+    }
+    public static void main(String[] args) {
+        final MessageDigest digest; 
+        String str = "Hello world";
+        try {
+            digest= MessageDigest.getInstance("SHA3-256");
+        } catch (NoSuchAlgorithmException e){
+            throw new RuntimeException(e);
+        }
+        final byte[] hashed = digest.digest(str.getBytes(StandardCharsets.UTF_8));
+        String sha3Hex = bytesToHex(hashed);
+        System.out.println(sha3Hex);
+    }
 }
